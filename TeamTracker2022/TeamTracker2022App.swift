7ca41f4938b8f16aff44582260aa74d9dc847394
pottsie <pottsma@me.com>
2022-12-13 15:31:24 -0500

    Initial Commit

---
+++ TeamTracker2022/TeamTracker2022App.swift
@@ -0,0 +1,17 @@
+//
+//  TeamTracker2022App.swift
+//  TeamTracker2022
+//
+//  Created by Michael Potts on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct TeamTracker2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
