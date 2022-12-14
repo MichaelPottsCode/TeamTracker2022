0b10855b800587d8d36562d87c8afd9669b07066
pottsie <pottsma@me.com>
2022-12-14 08:07:56 -0500

    Continued development of the app structure

---
+++ TeamTracker2022/Resources/Symbols.swift
@@ -0,0 +1,16 @@
+//
+//  Symbols.swift
+//  TeamTracker2022
+//
+//  Created by Michael Potts on 12/14/22.
+//
+
+import Foundation
+import SwiftUI
+
+enum Symbols {
+    
+    static let settings = Image(systemName: "gear")
+    static let profile = Image(systemName: "person")
+
+}
---
+++ TeamTracker2022/Resources/Theme.swift
@@ -0,0 +1,15 @@
+//
+//  Theme.swift
+//  TeamTracker2022
+//
+//  Created by Michael Potts on 12/14/22.
+//
+
+import Foundation
+import SwiftUI
+
+enum Theme {
+    
+    
+    
+}
