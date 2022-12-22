9643af348c6db5f9de9fc8e3ea44959f80870fb7
pottsie <pottsma@me.com>
2022-12-22 14:39:40 -0500

    Start of the player detail view features

---
+++ TeamTracker2022/Features/Player Detail/Views/PlayerDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  PlayerDetailView.swift
+//  TeamTracker2022
+//
+//  Created by Michael Potts on 12/22/22.
+//
+
+import SwiftUI
+
+struct PlayerDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PlayerDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        PlayerDetailView()
+    }
+}
